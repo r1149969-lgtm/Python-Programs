24657a398c9e56814fb030da3ceca1d24354e619
Kavya R <r1149969@gmail.com>
2026-06-25 11:11:16 +0530

    Added Python basic programs

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Student details program
+
+student_name="Kavya R"
+age = 22
+city = "Davanagere"
+percentage = 85.5
+
+print("Name:",student_name)
+print("Age:",age)
+print("City:",city)
+print("Percentage:",percentage)
